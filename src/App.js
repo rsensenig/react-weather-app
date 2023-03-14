00d2d7037a3f0c37b9585295e775e58cf7124cab
rsensenig <r.a.sensenig@gmail.com>
2023-03-13 22:56:21 -0400

    create html structure of App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const locationList = [
+  const cityList = [
     {
       key: '001',
       englishName: 'Shanghai',
@@ -35,7 +35,10 @@ function App() {
   
   return (
     <div className="App">
-      
+      <h1></h1>
+      <label></label>
+      <input></input>
+      <hr></hr>
     </div>
   );
 }
