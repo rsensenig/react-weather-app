7b56d37c904c5692304ee2186142388491e5abc7
rsensenig <r.a.sensenig@gmail.com>
2023-03-13 22:54:41 -0400

    create list of cities

--- src/App.js
+++ src/App.js
@@ -2,22 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const locationList = [
+    {
+      key: '001',
+      englishName: 'Shanghai',
+      primaryPostalCode: '200032',
+      country: {
+        id: 'CHN',
+        englishName: 'China'
+      },
+      administrativeArea: {
+        id: 'SH',
+        englishName: 'Shanghai Municipality',
+        englishType: 'Municipality'
+      }
+    },
+    {
+      key: '002',
+      englishName: 'Chicago',
+      primaryPostalCode: '60290',
+      country: {
+        id: 'USA',
+        englishName: 'United States of Amercia'
+      },
+      administrativeArea: {
+        id: 'IL',
+        englishName: 'Illinois',
+        englishType: 'State'
+      }
+    }
+  ]
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
